b26361b5e7164212638bf2660984daac0cfa9e1f
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-22 22:41:54 +0200

    Add styles for the Gifs

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./styles.css";
 
 export default function Gif({ title, id, url }) {
   return (
-    <div>
+    <a href={`#${id}`} className="Gif">
       <h4>
         {title}
       </h4>
       <img src={url} alt={title} />
-    </div>
+    </a>
   );
 }
