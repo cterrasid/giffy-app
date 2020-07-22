46146352262c6f658492da01b3b1dfccd92c7723
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-22 22:15:17 +0200

    Create components for the gif and the list of gifs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import ListOfGifs from "./components/ListOfGifs";
 import "./App.css";
-import getGifsService from "./services/getGifsService";
 
 function App() {
-  const [gifs, setGifs] = useState([]);
-
-  useEffect(() => {
-    getGifsService({ keyword: "rick" }).then(gifs => setGifs(gifs));
-  }, []);
-  
   return (
     <div className="App">
       <section className="App-content">
-        {gifs.map(gif => {
-          return (
-            <div key={gif.id}>
-              <h4>
-                {gif.title}
-              </h4>
-              <img src={gif.url} alt={gif.title} />
-            </div>
-          );
-        })}
+        <ListOfGifs keyword="panda" />
       </section>
     </div>
   );
