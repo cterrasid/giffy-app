99cb629fd4e96efc3406c9399b813ef6b47e14d7
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-22 22:32:47 +0200

    Save the keyword in the state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ListOfGifs from "./components/ListOfGifs";
 import "./App.css";
 
 function App() {
+  const [keyword, setKeyword] = useState("matrix");
+  
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword="panda" />
+        <ListOfGifs keyword={keyword} />
       </section>
     </div>
   );
