9089fa23279fe388c3dd21a59cacc2e1c5e19c54
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-22 21:48:41 +0200

    Call the Giphy api and paint the data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import getGifsService from "./services/getGifsService";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    getGifsService({ keyword: "rick" }).then(gifs => setGifs(gifs));
+  }, []);
   
   return (
     <div className="App">
       <section className="App-content">
-        <p>Clarette la mejor</p>
+        {gifs.map(gif => {
+          return (
+            <div key={gif.id}>
+              <h4>
+                {gif.title}
+              </h4>
+              <img src={gif.url} alt={gif.title} />
+            </div>
+          );
+        })}
       </section>
     </div>
   );
---
+++ src/services/getGifsService.js
@@ -0,0 +1,27 @@
+import { API_KEY, API_URL } from "./settings";
+
+const fromApiResponseToGifs = apiResponse => {
+  const { data = [] } = apiResponse;
+
+  if (Array.isArray(data)) {
+    const gifs = data.map(image => {
+      const { id, title, images } = image;
+      const { url } = images.downsized_medium;
+
+      return { title, id, url };
+    });
+    return gifs;
+  }
+  return [];
+};
+
+export default async function getGifsService(
+  { limit = 5, keyword = "panda", page = 0 } = {}
+) {
+  const url = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page *
+    limit}&rating=g&lang=en`;
+  const res = await fetch(url);
+  const response = await res.json().then(fromApiResponseToGifs);
+
+  return response;
+}
