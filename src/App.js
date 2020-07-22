8d4fbade9bcbcd97412b74cf102ca4244995b152
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-22 20:58:34 +0200

    Set the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <section className="App-content">
+        <p>Clarette la mejor</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
