4782d3cb3f8fed986017e1d386e5a4b7888346de
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-24 15:57:53 +0200

    Show gif detail using data from the context

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,14 +1,11 @@
 import React, { useContext } from "react";
 import GifsContext from "../../context/GifsContext";
-// import Gif from "../../components/Gif";
+import Gif from "../../components/Gif";
 
 export default function Detail({ params }) {
   const { gifs } = useContext(GifsContext);
-  console.log(gifs);
-  return (
-    <h1>
-      Gif id {params.id}
-    </h1>
-  );
-  //   return <Gif id={id} title={title} url={url} />;
+
+  const gif = gifs.find(gif => gif.id === params.id);
+
+  return <Gif {...gif} />;
 }
