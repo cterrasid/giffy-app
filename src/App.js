e84c991c83264a958fbbe816252275142d922583
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-24 15:03:05 +0200

    Wrap the routes with the context's provider

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import Detail from "./pages/Detail";
 import "./App.css";
+import { GifsContextProvider } from "./context/GifsContext";
 
 function App() {
   return (
     <div className="App">
       <section className="App-content">
         <Route path="/" component={Home} />
-        <Route path="/search/:keyword" component={SearchResults} />
-        <Route path="/gif/:id" component={Detail} />
+        <GifsContextProvider>
+          <Route path="/search/:keyword" component={SearchResults} />
+          <Route path="/gif/:id" component={Detail} />
+        </GifsContextProvider>
       </section>
     </div>
   );
