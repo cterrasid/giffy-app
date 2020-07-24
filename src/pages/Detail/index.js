0461f7044bb202f1f112876630740bcbbfdae67d
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-24 15:03:32 +0200

    Call the context in the Detail page

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,7 +1,14 @@
-import React from "react";
-import Gif from "../../components/Gif";
+import React, { useContext } from "react";
+import GifsContext from "../../context/GifsContext";
+// import Gif from "../../components/Gif";
 
-export default function Detail({params}) {
-    return <h1>Gif id {params.id}</h1>
-//   return <Gif id={id} title={title} url={url} />;
+export default function Detail({ params }) {
+  const { gifs } = useContext(GifsContext);
+  console.log(gifs);
+  return (
+    <h1>
+      Gif id {params.id}
+    </h1>
+  );
+  //   return <Gif id={id} title={title} url={url} />;
 }
