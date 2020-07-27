211ef229e08f2bf1aacb0ffc8850e95711b96da0
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-27 20:04:13 +0200

    Add Intersection Observer to the Trending list

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import getTrendingSearchesService from "services/getTrendingSearchesService";
 import ListOfCategories from "../ListOfCategories";
 
-export default function TrendingSearches() {
+function TrendingSearches() {
   const [trends, setTrends] = useState([]);
 
   useEffect(() => {
@@ -10,4 +10,26 @@ export default function TrendingSearches() {
   }, []);
 
   return <ListOfCategories name="Trends" options={trends} />;
+}
+
+export default function LazyTrending() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const onChange = entries => {
+      const el = entries[0];
+      el.isIntersecting && setShow(true);
+    };
+
+    const observer = new IntersectionObserver(onChange, {
+      rootMargin: "100px"
+    });
+
+    observer.observe(document.getElementById("LazyTrending"));
+  });
+  return (
+    <div id="LazyTrending">
+      {show ? <TrendingSearches /> : null}
+    </div>
+  );
 }
