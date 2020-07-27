c1aa522395045f7361d56c95177226c1a0e2a74d
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-27 18:53:47 +0200

    Add decodeURI to add spaces to the searches title

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ListOfGifs from "../../components/ListOfGifs";
 import useGifs from "../../hooks/useGifs";
 
 export default function SearchResults({ params }) {
   const { keyword } = params;
 
-  const { isLoading, gifs } = useGifs({keyword});
+  const { isLoading, gifs } = useGifs({ keyword });
 
-  return isLoading ? "Loading" : <ListOfGifs gifs={gifs} />;
+  return isLoading
+    ? "Loading"
+    : <Fragment>
+        <h3>
+          {decodeURI(keyword)}
+        </h3>
+        <ListOfGifs gifs={gifs} />
+      </Fragment>;
 }
