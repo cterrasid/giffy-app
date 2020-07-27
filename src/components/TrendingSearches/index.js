c6c990d6bdb1dcc6e4e22d96901c20cd06a2265d
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-27 20:48:13 +0200

    Add polifyll of intersection observer to support all the browsers

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -17,22 +17,30 @@ export default function LazyTrending() {
   const elementRef = useRef();
 
   useEffect(() => {
+    let observer;
     const onChange = (entries, observer) => {
       const el = entries[0];
       el.isIntersecting && setShow(true);
       observer.disconnect();
     };
 
-    const observer = new IntersectionObserver(onChange, {
-      rootMargin: "100px"
+    Promise.resolve(
+      typeof IntersectionObserver !== undefined
+        ? IntersectionObserver
+        : import("intersection-observer")
+    ).then(() => {
+      observer = new IntersectionObserver(onChange, {
+        rootMargin: "100px"
+      });
+
+      observer.observe(elementRef.current);
     });
 
-    observer.observe(elementRef.current);
-
-    return () => observer.disconnect();
+    return () => observer && observer.disconnect();
   });
+
   return (
-    <div ref="elementRef">
+    <div ref={elementRef}>
       {show ? <TrendingSearches /> : null}
     </div>
   );
