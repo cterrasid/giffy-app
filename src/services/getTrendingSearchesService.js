662102a67585eac399bd25babf78f376e87fb93d
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-31 23:56:26 +0200

    Fix trending searches url

--- src/services/getTrendingSearchesService.js
+++ src/services/getTrendingSearchesService.js
@@ -5,11 +5,8 @@ const fromApiResponseToGifs = apiResponse => {
   return data;
 };
 
-export default async function getTrendingSearchesService(
-  { limit = 25, keyword = "panda", page = 0 } = {}
-) {
-  const url = `${API_URL}/trending/searches?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page *
-    limit}&rating=g&lang=en`;
+export default async function getTrendingSearchesService() {
+  const url = `${API_URL}/trending/searches?api_key=${API_KEY}`;
   const res = await fetch(url);
   const response = await res.json().then(fromApiResponseToGifs);
 
