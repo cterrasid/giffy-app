6260ee0e17532be1f7c6b1b53a136bee7f9fabbb
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-08-09 01:04:12 +0200

    Apply tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import "intersection-observer";
+import {
+  render,
+  waitForElement,
+  fireEvent,
+  screen,
+} from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Test para verificar si existe un elemento en una ruta asincrona (con code splitting)
+test("renders without crashing", async () => {
+  const { findByText } = render(<App />);
+  const title = await findByText(/Última búsqueda/i);
+  expect(title).toBeInTheDocument();
+});
+
+test("home work as expected", async () => {
+  const { container } = render(<App />);
+  const gifLink = await waitForElement(() =>
+    container.querySelector(".Gif-link")
+  );
+  expect(gifLink).toBeVisible();
+});
+
+test("search form could be used", async () => {
+  render(<App />);
+  const input = await screen.findByRole("textbox");
+  const button = await screen.findByRole("button");
+
+  fireEvent.change(input, { target: { value: "Matrix" } });
+  fireEvent.click(button);
+
+  const title = await screen.findByText("Matrix");
+  expect(title).toBeVisible();
 });
