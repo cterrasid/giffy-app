6997722ce2b2553d0bbc43023e95cc9047f76793
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-08-08 23:41:51 +0200

    Add code splitting to the homepage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route } from "wouter";
-import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import Detail from "./pages/Detail";
 import "./App.css";
 import { GifsContextProvider } from "./context/GifsContext";
 
+const HomePage = lazy(() => import("./pages/Home"));
+
 function App() {
   return (
     <div className="App">
-      <section className="App-content">
-        <GifsContextProvider>
-          <Route path="/" component={Home} />
-          <Route path="/search/:keyword" component={SearchResults} />
-          <Route path="/gif/:id" component={Detail} />
-        </GifsContextProvider>
-      </section>
+      <Suspense fallback={null}>
+        <section className="App-content">
+          <GifsContextProvider>
+            <Route path="/" component={HomePage} />
+            <Route path="/search/:keyword" component={SearchResults} />
+            <Route path="/gif/:id" component={Detail} />
+          </GifsContextProvider>
+        </section>
+      </Suspense>
     </div>
   );
 }
