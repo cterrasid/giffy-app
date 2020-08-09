4465e5c996ea7ad2bcedda3ae787622fbf5a70b2
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-08-09 23:52:53 +0200

    Add react.memo to the Gif components

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { memo } from "react";
 import { Link } from "wouter";
 import "./styles.css";
 
-export default function Gif({ title, id, url }) {
+function Gif({ title, id, url }) {
   return (
     <article className="Gif">
       <Link to={`/gif/${id}`} className="Gif-link">
-        <h4>
-          {title}
-        </h4>
+        <h4>{title}</h4>
         <img loading="lazy" src={url} alt={title} />
       </Link>
     </article>
   );
 }
+
+export default memo(Gif, (prevProps, nextProps) => {
+  return prevProps.id === nextProps.id;
+});
