cc3e5e3c5bbd34c5f7cbfff651b274805d4c8f2e
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-09-14 17:05:31 +0200

    Added window size hook

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect } from "react";
-import getGifsService from "../services/getGifsService";
-import GifsContext from "../context/GifsContext";
+import getGifsService from "services/getGifsService";
+import GifsContext from "context/GifsContext";
+import useWindowSize from "./useWindowSize";
 
 const INITIAL_PAGE = 0;
 
@@ -8,6 +9,8 @@ export default function useGifs({ keyword, rating } = { keyword: null }) {
   const [isLoading, setIsLoading] = useState(false);
   const [isLoadingNextPage, setIsLoadingNextPage] = useState(false);
   const [page, setPage] = useState(INITIAL_PAGE);
+
+  const { isTablet } = useWindowSize();
 
   // Actualizar el estado del contexto, en lugar del estado local:
   const { gifs, setGifs } = useContext(GifsContext);
@@ -18,12 +21,16 @@ export default function useGifs({ keyword, rating } = { keyword: null }) {
   useEffect(() => {
     setIsLoading(true);
 
-    getGifsService({ keyword: keywordToUse, rating }).then((gifs) => {
+    getGifsService({
+      keyword: keywordToUse,
+      rating,
+      limit: isTablet ? 27 : 30,
+    }).then((gifs) => {
       setGifs(gifs);
       setIsLoading(false);
       localStorage.setItem("lastKeyword", keyword);
     });
-  }, [keyword, keywordToUse, rating, setGifs]);
+  }, [keyword, keywordToUse, rating, setGifs, isTablet]);
 
   useEffect(() => {
     if (page === INITIAL_PAGE) return;
